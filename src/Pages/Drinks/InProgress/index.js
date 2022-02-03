9d5399dc3e6551390a208153dd68ebf6d038fff9
HiagoBriano <hiago.artist@hotmail.com>
2022-02-03 11:57:18 -0300

    pega dados id da URL e busca receita na api

--- src/Pages/Drinks/InProgress/index.js
+++ src/Pages/Drinks/InProgress/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect, useState } from 'react';
+import context from '../../../Context/Context';
 
-function InProgressDrinks() {
+function InProgressDrinks({ history }) {
+  const [response, setResponse] = useState([]);
+  const { api } = useContext(context);
+
+  useEffect(() => {
+    (async () => {
+      // pegar id do URL
+      const { pathname } = history.location;
+      const FIXO = 7;
+      const id = pathname.substring(pathname.lastIndexOf('drinks/') + FIXO).split('/')[0];
+      // pegar dados da API
+      const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+      console.log(URL);
+      const { drinks } = await api(URL);
+      setResponse(drinks[0]);
+    })();
+  }, []);
+
   return (
     <div>
       <p>InProgressDrinks</p>
+      {response ? <h3>{response.strDrink}</h3> : null}
     </div>
   );
 }
+
+InProgressDrinks.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.func,
+  }).isRequired,
+};
 
 export default InProgressDrinks;
--- src/Pages/Foods/InProgress/index.js
+++ src/Pages/Foods/InProgress/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect, useState } from 'react';
+import context from '../../../Context/Context';
 
-function InProgressFoods() {
+function InProgressFoods({ history }) {
+  const [response, setResponse] = useState([]);
+  const { api } = useContext(context);
+
+  useEffect(() => {
+    (async () => {
+      // pegar id do URL
+      const { pathname } = history.location;
+      const FIXO = 6;
+      const id = pathname.substring(pathname.lastIndexOf('foods/') + FIXO).split('/')[0];
+      // pegar dados da API
+      const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+      console.log(URL);
+      const { meals } = await api(URL);
+      setResponse(meals[0]);
+    })();
+  }, []);
+
   return (
     <div>
       <p>InProgressFoods</p>
+      {response ? <h3>{response.strMeal}</h3> : null}
     </div>
   );
 }
+
+InProgressFoods.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.func,
+  }).isRequired,
+};
 
 export default InProgressFoods;
